82fa4da511aa681b4fa47a5358e6ca1efc65fc40
Ian Vernon <ian@cilium.io>
2019-04-05 14:14:21 -0700

    add identitymanager package
    This package will be used to track what global identities are being consumed
    by the currently running cilium-agent, i.e. what identities are being used
    by endpoints that are currently running.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

---
+++ pkg/identity/identitymanager/manager.go
@@ -0,0 +1,92 @@
+// Copyright 2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package identitymanager
+
+import (
+	"github.com/cilium/cilium/pkg/identity"
+	"github.com/cilium/cilium/pkg/lock"
+)
+
+var (
+	// GlobalIdentityManager is a singleton instance of an IdentityManager, used
+	// for easy updating / tracking lifecycles of identities on the local node
+	// without having to pass around a specific instance of an IdentityManager
+	// throughout Cilium.
+	GlobalIdentityManager = NewIdentityManager()
+)
+
+// IdentityManager caches information about a set of identities, currently a
+// reference count of how many users there are for each identity.
+type IdentityManager struct {
+	mutex      lock.RWMutex
+	identities map[*identity.Identity]*identityMetadata
+}
+
+type identityMetadata struct {
+	refCount uint
+}
+
+// NewIdentityManager returns an initialized IdentityManager.
+func NewIdentityManager() *IdentityManager {
+	return &IdentityManager{
+		identities: make(map[*identity.Identity]*identityMetadata),
+	}
+}
+
+// Upsert inserts the identity into the GlobalIdentityManager.
+func Upsert(identity *identity.Identity) {
+	GlobalIdentityManager.Upsert(identity)
+}
+
+// Delete deletes the identity from the GlobalIdentityManager.
+func Delete(identity *identity.Identity) {
+	GlobalIdentityManager.Delete(identity)
+}
+
+// Upsert inserts the identity into the identity manager. If the identity is
+// already in the identity manager, the reference count for the identity is
+// incremented.
+func (idm *IdentityManager) Upsert(identity *identity.Identity) {
+	idm.mutex.Lock()
+	defer idm.mutex.Unlock()
+
+	idMeta, exists := idm.identities[identity]
+	if !exists {
+		idm.identities[identity] = &identityMetadata{
+			refCount: 1,
+		}
+	} else {
+		idMeta.refCount++
+	}
+}
+
+// Delete deletes the identity from the identity manager. If the identity is
+// already in the identity manager, the reference count for the identity is
+// decremented. If the identity is not in the cache, this is a no-op. If the
+// ref count becomes zero, the identity is removed from the cache.
+func (idm *IdentityManager) Delete(identity *identity.Identity) {
+	idm.mutex.Lock()
+	defer idm.mutex.Unlock()
+
+	idMeta, exists := idm.identities[identity]
+	if !exists {
+		return
+	}
+	idMeta.refCount--
+	if idMeta.refCount == 0 {
+		delete(idm.identities, identity)
+	}
+
+}
