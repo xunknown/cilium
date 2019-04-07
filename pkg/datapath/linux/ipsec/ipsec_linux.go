961847d5256fdb29d351df6ab91a9a910d07d28b
John Fastabend <john.fastabend@gmail.com>
2019-04-06 20:15:13 -0700

    cilium: support aead state keys
    Add support aead keys.
    
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

--- pkg/datapath/linux/ipsec/ipsec_linux.go
+++ pkg/datapath/linux/ipsec/ipsec_linux.go
@@ -48,6 +48,7 @@ type ipSecKey struct {
 	ReqID int
 	Auth  *netlink.XfrmStateAlgo
 	Crypt *netlink.XfrmStateAlgo
+	Aead  *netlink.XfrmStateAlgo
 }
 
 // ipSecKeysGlobal is safe to read unlocked because the only writers are from
@@ -90,8 +91,12 @@ func ipSecAttachPolicyTempl(policy *netlink.XfrmPolicy, keys *ipSecKey, srcIP, d
 }
 
 func ipSecJoinState(state *netlink.XfrmState, keys *ipSecKey) {
-	state.Auth = keys.Auth
-	state.Crypt = keys.Crypt
+	if keys.Aead != nil {
+		state.Aead = keys.Aead
+	} else {
+		state.Crypt = keys.Crypt
+		state.Auth = keys.Auth
+	}
 	state.Spi = int(keys.Spi)
 	state.Reqid = keys.ReqID
 }
@@ -333,7 +338,7 @@ func loadIPSecKeys(r io.Reader) (uint8, error) {
 		// Scanning IPsec keys formatted as follows,
 		//    auth-algo auth-key enc-algo enc-key
 		s := strings.Split(scanner.Text(), " ")
-		if len(s) < 4 {
+		if len(s) < 2 {
 			return 0, fmt.Errorf("missing IPSec keys or invalid format")
 		}
 
@@ -358,20 +363,29 @@ func loadIPSecKeys(r io.Reader) (uint8, error) {
 		}
 		authname := s[1+offset]
 
-		enckey, err := decodeIPSecKey(s[4+offset])
-		if err != nil {
-			return 0, fmt.Errorf("unable to decode enckey string %q", s[3+offset])
-		}
-		encname := s[3+offset]
+		if strings.HasPrefix(authname, "rfc") {
+			ipSecKey.Aead = &netlink.XfrmStateAlgo{
+				Name:   authname,
+				Key:    authkey,
+				ICVLen: 128,
+			}
+		} else {
+			enckey, err := decodeIPSecKey(s[4+offset])
+			if err != nil {
+				return 0, fmt.Errorf("unable to decode enckey string %q", s[3+offset])
+			}
+			encname := s[3+offset]
 
-		ipSecKey.Auth = &netlink.XfrmStateAlgo{
-			Name: authname,
-			Key:  authkey,
+			ipSecKey.Auth = &netlink.XfrmStateAlgo{
+				Name: authname,
+				Key:  authkey,
+			}
+			ipSecKey.Crypt = &netlink.XfrmStateAlgo{
+				Name: encname,
+				Key:  enckey,
+			}
 		}
-		ipSecKey.Crypt = &netlink.XfrmStateAlgo{
-			Name: encname,
-			Key:  enckey,
-		}
+
 		ipSecKey.Spi = spi
 
 		if len(s) == 6+offset {
