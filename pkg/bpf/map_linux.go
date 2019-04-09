d1c13ddc1e9c8d35b25a35d5d7b040efc456cd97
André Martins <andre@cilium.io>
2019-04-09 19:31:28 +0200

    wip2
    Signed-off-by: André Martins <andre@cilium.io>

--- pkg/bpf/map_linux.go
+++ pkg/bpf/map_linux.go
@@ -18,12 +18,13 @@ package bpf
 
 import (
 	"bufio"
-	"bytes"
 	"context"
 	"encoding/binary"
 	"fmt"
+	"math"
 	"os"
 	"path"
+	"reflect"
 	"sync"
 	"syscall"
 	"time"
@@ -851,7 +852,7 @@ func (m *Map) DeleteAll() error {
 	return nil
 }
 
-//GetNextKey returns the next key in the Map after key.
+// GetNextKey returns the next key in the Map after key.
 func (m *Map) GetNextKey(key MapKey, nextKey MapKey) error {
 	if err := m.Open(); err != nil {
 		return err
@@ -862,19 +863,191 @@ func (m *Map) GetNextKey(key MapKey, nextKey MapKey) error {
 	return err
 }
 
+type coder struct {
+	order  binary.ByteOrder
+	buf    []byte
+	offset int
+}
+
+type decoder coder
+
+func (d *decoder) bool() bool {
+	x := d.buf[d.offset]
+	d.offset++
+	return x != 0
+}
+
+func (d *decoder) uint8() uint8 {
+	x := d.buf[d.offset]
+	d.offset++
+	return x
+}
+
+func (d *decoder) uint16() uint16 {
+	x := d.order.Uint16(d.buf[d.offset : d.offset+2])
+	d.offset += 2
+	return x
+}
+
+func (d *decoder) uint32() uint32 {
+	x := d.order.Uint32(d.buf[d.offset : d.offset+4])
+	d.offset += 4
+	return x
+}
+
+func (d *decoder) uint64() uint64 {
+	x := d.order.Uint64(d.buf[d.offset : d.offset+8])
+	d.offset += 8
+	return x
+}
+
+func (d *decoder) skip(v reflect.Value) {
+	d.offset += dataSize(v)
+}
+
+// sizeof returns the size >= 0 of variables for the given type or -1 if the type is not acceptable.
+func sizeof(t reflect.Type) int {
+	switch t.Kind() {
+	case reflect.Array:
+		if s := sizeof(t.Elem()); s >= 0 {
+			return s * t.Len()
+		}
+
+	case reflect.Struct:
+		sum := 0
+		for i, n := 0, t.NumField(); i < n; i++ {
+			s := sizeof(t.Field(i).Type)
+			if s < 0 {
+				return -1
+			}
+			sum += s
+		}
+		return sum
+
+	case reflect.Bool,
+		reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Float32, reflect.Float64, reflect.Complex64, reflect.Complex128:
+		return int(t.Size())
+	}
+
+	return -1
+}
+
+// dataSize returns the number of bytes the actual data represented by v occupies in memory.
+// For compound structures, it sums the sizes of the elements. Thus, for instance, for a slice
+// it returns the length of the slice times the element size and does not count the memory
+// occupied by the header. If the type of v is not acceptable, dataSize returns -1.
+func dataSize(v reflect.Value) int {
+	if v.Kind() == reflect.Slice {
+		if s := sizeof(v.Type().Elem()); s >= 0 {
+			return s * v.Len()
+		}
+		return -1
+	}
+	return sizeof(v.Type())
+}
+
+func (d *decoder) int8() int8 { return int8(d.uint8()) }
+
+func (d *decoder) int16() int16 { return int16(d.uint16()) }
+
+func (d *decoder) int32() int32 { return int32(d.uint32()) }
+
+func (d *decoder) int64() int64 { return int64(d.uint64()) }
+
+func (d *decoder) value(v reflect.Value) {
+	switch v.Kind() {
+	case reflect.Array:
+		l := v.Len()
+		for i := 0; i < l; i++ {
+			d.value(v.Index(i))
+		}
+
+	case reflect.Struct:
+		t := v.Type()
+		l := v.NumField()
+		for i := 0; i < l; i++ {
+			// Note: Calling v.CanSet() below is an optimization.
+			// It would be sufficient to check the field name,
+			// but creating the StructField info for each field is
+			// costly (run "go test -bench=ReadStruct" and compare
+			// results when making changes to this code).
+			if v := v.Field(i); v.CanSet() || t.Field(i).Name != "_" {
+				d.value(v)
+			} else {
+				d.skip(v)
+			}
+		}
+
+	case reflect.Slice:
+		l := v.Len()
+		for i := 0; i < l; i++ {
+			d.value(v.Index(i))
+		}
+
+	case reflect.Bool:
+		v.SetBool(d.bool())
+
+	case reflect.Int8:
+		v.SetInt(int64(d.int8()))
+	case reflect.Int16:
+		v.SetInt(int64(d.int16()))
+	case reflect.Int32:
+		v.SetInt(int64(d.int32()))
+	case reflect.Int64:
+		v.SetInt(d.int64())
+
+	case reflect.Uint8:
+		v.SetUint(uint64(d.uint8()))
+	case reflect.Uint16:
+		v.SetUint(uint64(d.uint16()))
+	case reflect.Uint32:
+		v.SetUint(uint64(d.uint32()))
+	case reflect.Uint64:
+		v.SetUint(d.uint64())
+
+	case reflect.Float32:
+		v.SetFloat(float64(math.Float32frombits(d.uint32())))
+	case reflect.Float64:
+		v.SetFloat(math.Float64frombits(d.uint64()))
+
+	case reflect.Complex64:
+		v.SetComplex(complex(
+			float64(math.Float32frombits(d.uint32())),
+			float64(math.Float32frombits(d.uint32())),
+		))
+	case reflect.Complex128:
+		v.SetComplex(complex(
+			math.Float64frombits(d.uint64()),
+			math.Float64frombits(d.uint64()),
+		))
+	}
+}
+
+func read(r []byte, order binary.ByteOrder, data interface{}) error {
+	// Fallback to reflect-based decoding.
+	v := reflect.ValueOf(data)
+	switch v.Kind() {
+	case reflect.Ptr:
+		v = v.Elem()
+	}
+	d := &decoder{order: order, buf: r}
+	d.value(v)
+	return nil
+}
+
 // ConvertKeyValue converts key and value from bytes to given Golang struct pointers.
 func ConvertKeyValue(bKey []byte, bValue []byte, key MapKey, value MapValue) error {
-	keyBuf := bytes.NewBuffer(bKey)
-	valueBuf := bytes.NewBuffer(bValue)
 
 	if len(bKey) > 0 {
-		if err := binary.Read(keyBuf, byteorder.Native, key); err != nil {
+		if err := read(bKey, byteorder.Native, key); err != nil {
 			return fmt.Errorf("Unable to convert key: %s", err)
 		}
 	}
 
 	if len(bValue) > 0 {
-		if err := binary.Read(valueBuf, byteorder.Native, value); err != nil {
+		if err := read(bValue, byteorder.Native, value); err != nil {
 			return fmt.Errorf("Unable to convert value: %s", err)
 		}
 	}
